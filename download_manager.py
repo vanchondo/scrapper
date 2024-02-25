2e502d42703974ff012c0605cbd854ca13e5b160
Victor Anchondo <eduardoanchondo@gmail.com>
2024-02-24 19:43:04 -0600

    Removing rest endpoint

--- download_manager.py
+++ download_manager.py
@@ -1,22 +1,15 @@
+import os
 import shutil
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 from utils import (create_folder, create_pdf, download_file, get_all_chapters,
                    get_name)
 
 
-def download_chapter(root_folder, host_url, base_url, name, one_piece_id, chapter_start, chapter_end):
+def download_chapter(root_folder, host_url, base_url, one_piece_id, chapter_start, chapter_end, config):
     chapters = get_all_chapters(host_url + "chapter/getall?mangaIdentification=" + one_piece_id)
-
-    hub_url = "http://selenium-grid-driver:4444"
-    options = Options()
-
-    driver = webdriver.Remote(command_executor=hub_url, options=options)
-    driver.set_page_load_timeout(120)
-    driver.implicitly_wait(20)
 
     for chapter in chapters:
         chapter_number = chapter['FriendlyChapterNumberUrl']
@@ -30,6 +23,9 @@ def download_chapter(root_folder, host_url, base_url, name, one_piece_id, chapte
             chapter_folder = root_folder + "/" + get_name(chapter_number, "0000")
             create_folder(chapter_folder)
 
+            driver = webdriver.Firefox()
+            driver.implicitly_wait(10)
+            driver.maximize_window()
             driver.get(base_url + chapter_number + "/" + identification)
             pages = driver.find_elements(By.CLASS_NAME, "ImageContainer")
 
@@ -37,7 +33,7 @@ def download_chapter(root_folder, host_url, base_url, name, one_piece_id, chapte
 
             for page in pages:
                 page_id = page.get_attribute("id")
-                image_url = f"{host_url}images/manga/{name}/chapter/{chapter_number}/page/{current_page_number}/{page_id}"
+                image_url = f"{host_url}images/manga/One-Piece/chapter/{chapter_number}/page/{current_page_number}/{page_id}"
                 print(f"Downloading image from: {image_url}")
                 file_name = chapter_folder + "/" + get_name(str(current_page_number), "00") + ".jpeg"
                 download_file(image_url, file_name)
@@ -45,4 +41,4 @@ def download_chapter(root_folder, host_url, base_url, name, one_piece_id, chapte
 
             create_pdf(chapter_folder, chapter_folder + ".pdf")
             shutil.rmtree(chapter_folder)
-    driver.close()
+            driver.close()
--- main.py
+++ main.py
@@ -1,47 +1,25 @@
 import configparser
-import shutil
-import uuid
-
-from flask import Flask, send_file
 
 from download_manager import download_chapter
-from utils import create_folder, get_name
-
-app = Flask(__name__)
+from utils import create_folder, get_all_chapters
 
 # Read configuration from application.properties
 config = configparser.ConfigParser()
 config.read('application.properties')
 
+root_folder = config.get('Default', 'rootFolder')
 host_url = config.get('Default', 'hostUrl')
 base_url = host_url + config.get('Default', 'baseUrl')
+one_piece_id = config.get('Default', 'onePieceId')
+chapter_start = int(config.get('Default', 'chapterStart'))
+chapter_end = int(config.get('Default', 'chapterEnd'))
 
-@app.route('/download/name/<name>/id/<mangaId>/chapter/<chapter>', methods=['POST'])
-def download(name, mangaId, chapter):
-    print(f"Received request to download name={name}, mangaId={mangaId}, chapter={chapter}")
+# Create the root folder
+create_folder(root_folder)
 
-    url = base_url + name + "/"
+print("Get all chapters...")
+chapters = get_all_chapters(host_url + "chapter/getall?mangaIdentification=" + one_piece_id)
+print(f"Found {len(chapters)} chapters")
 
-    # Generate a UUID
-    new_uuid = uuid.uuid4()
-    # Convert the UUID to a string
-    uuid_string = str(new_uuid)
-
-    root_folder = name + "-" + uuid_string
-    # Download chapters within the specified range
-    download_chapter(root_folder, host_url, url, name, mangaId, int(chapter), int(chapter))
-
-    # Specify the path to your PDF file
-    pdf_path = root_folder + "/" + get_name(chapter, "0000") + ".pdf"
-
-    # Return the PDF file as a response
-    response = send_file(pdf_path, as_attachment=True)
-
-    # Delete folder
-    shutil.rmtree(root_folder)
-
-    return response
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6066)
+# Download chapters within the specified range
+download_chapter(root_folder, host_url, base_url, one_piece_id, chapter_start, chapter_end, config)
