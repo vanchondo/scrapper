df540a1b01c840fe49c030abff61308c01b2ed86
Victor Anchondo <eduardoanchondo@gmail.com>
2024-03-20 15:52:44 -0600

    Adding arguments to control what to download

--- download_manager.py
+++ download_manager.py
@@ -8,12 +8,16 @@ from utils import (create_folder, create_pdf, download_file, get_all_chapters,
                    get_name)
 
 
-def download_chapter(root_folder, host_url, base_url, one_piece_id, chapter_start, chapter_end, config):
-    chapters = get_all_chapters(host_url + "chapter/getall?mangaIdentification=" + one_piece_id)
+def download_chapter(manga_name, inManga_url, resources_host_url, base_url, get_all_url, manga_id, chapter_start, chapter_end, config):
+    chapters = get_all_chapters(inManga_url + get_all_url + manga_id)
 
     # If chapter_end was not provided, use the latest chapter as end.
     if (chapter_end == 0):
         chapter_end = chapters[-1]['FriendlyChapterNumberUrl']
+
+    # If chapter_start was not provided, use the latest chapter as end.
+    if (chapter_start == 0):
+        chapter_start = chapters[-1]['FriendlyChapterNumberUrl']
 
     print(f"Trying to download from chapter {chapter_start} to {chapter_end}")
 
@@ -26,7 +30,7 @@ def download_chapter(root_folder, host_url, base_url, one_piece_id, chapter_star
             page_count = chapter['PagesCount']
             print(f"Chapter={current_chapter} pages={page_count}")
 
-            chapter_folder = root_folder + "/" + get_name(chapter_number, "0000")
+            chapter_folder = manga_name + " v" + get_name(chapter_number, "0000")
             create_folder(chapter_folder)
 
             driver = webdriver.Firefox()
@@ -39,7 +43,7 @@ def download_chapter(root_folder, host_url, base_url, one_piece_id, chapter_star
 
             for page in pages:
                 page_id = page.get_attribute("id")
-                image_url = f"{host_url}images/manga/One-Piece/chapter/{chapter_number}/page/{current_page_number}/{page_id}"
+                image_url = f"{resources_host_url}images/manga/{manga_name}/chapter/{chapter_number}/page/{current_page_number}/{page_id}"
                 print(f"Downloading image from: {image_url}")
                 file_name = chapter_folder + "/" + get_name(str(current_page_number), "00") + ".jpeg"
                 download_file(image_url, file_name)
--- main.py
+++ main.py
@@ -1,25 +1,37 @@
+import argparse
 import configparser
 
 from download_manager import download_chapter
 from utils import create_folder, get_all_chapters
 
+# Create an ArgumentParser object
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+# Add arguments to the parser
+parser.add_argument('--mangaName', type=str, help='Manga name', required=True)
+parser.add_argument('--mangaId', type=str, help='Manga Id', required=True)
+parser.add_argument('--chapterStart', type=int, help='Chapter start', default=0)
+parser.add_argument('--chapterEnd', type=int, help='Chapter end', default=0)
+
+# Parse the command-line arguments
+args = parser.parse_args()
+manga_name = args.mangaName
+manga_id = args.mangaId
+chapter_start = args.chapterStart
+chapter_end = args.chapterEnd
+
 # Read configuration from application.properties
 config = configparser.ConfigParser()
 config.read('application.properties')
 
-root_folder = config.get('Default', 'rootFolder')
-host_url = config.get('Default', 'hostUrl')
-base_url = host_url + config.get('Default', 'baseUrl')
-one_piece_id = config.get('Default', 'onePieceId')
-chapter_start = int(config.get('Default', 'chapterStart'))
-chapter_end = int(config.get('Default', 'chapterEnd'))
-
-# Create the root folder
-create_folder(root_folder)
+resources_host_url = config.get('Default', 'hostUrl')
+base_url = resources_host_url + config.get('Default', 'baseUrl') + manga_name + "/"
+inManga_url = config.get('Default', 'inMangaHostUrl')
+get_all_url = config.get('Default', 'getAllUrl')
 
 print("Get all chapters...")
-chapters = get_all_chapters(host_url + "chapter/getall?mangaIdentification=" + one_piece_id)
+chapters = get_all_chapters(inManga_url + get_all_url + manga_id)
 print(f"Found {len(chapters)} chapters")
 
 # Download chapters within the specified range
-download_chapter(root_folder, host_url, base_url, one_piece_id, chapter_start, chapter_end, config)
+download_chapter(manga_name, inManga_url, resources_host_url, base_url, get_all_url, manga_id, chapter_start, chapter_end, config)
